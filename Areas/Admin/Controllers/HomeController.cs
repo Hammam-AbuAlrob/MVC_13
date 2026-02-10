f9823de61bc55898988481e570fc047d3d9c2a1a
Hammam-AbuAlrob <cohammam@gmail.com>
2026-02-11 01:20:47 +0200

    first commit

---
+++ Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KAShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return Content("this is admin home");
+
+            //return View("Index")
+            //or return View();
+            return View("Index");
+        }
+    }
+}
---
+++ Areas/User/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KAShop.Areas.User.Controllers
+{
+    [Area("User")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+          //  return Content("this is home for user ");
+        }
+    }
+}
